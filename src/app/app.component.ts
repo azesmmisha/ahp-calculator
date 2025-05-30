8d0d5ba4133d3208f26d26cbd6a6ba2c18d9d40b
Mykhailo Oliinyk <mykhailooliinyk@MacBook-Pro-Mykhailo.local>
2025-05-30 16:05:16 +0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -20,6 +21,11 @@ interface WeightResult {
     geomMeanNormalized: number;
 }
 
+interface IComparison {
+    entity: string;
+    importances: Importance[];
+}
+
 interface Importance {
     entity: string;
     value: number;
@@ -27,15 +33,35 @@ interface Importance {
     disabled: boolean;
 }
 
-interface IComparison {
-    entity: string;
-    importances: Importance[];
+interface ICrCompMatrix {
+    compValues: IComparison[];
+    processed: boolean;
+    consistent: boolean;
+    priorityVector: string;
+    result: WeightResult[];
+    weightedSumVector: number[];
+    consistencyVector: number[];
+    lambdaMax: number;
+    ci: number;
+    ri: number;
+    cr: number;
+    threshold: number;
 }
 
-interface IComparisonByCriterion {
-    criterion: string;
+interface IAltComparison {
+    processed: boolean;
     consistent: boolean;
-    comparisons: IComparison[];
+    matrixes: IAltCompMatrix[];
+}
+
+interface IAltCompMatrix extends ICrCompMatrix {
+    criterion: string;
+}
+
+interface IResult {
+    alternative: string;
+    priorityVector: number[];
+    result: number;
 }
 
 type MarkLabel = '1/9' | '1/8' | '1/7' | '1/6' | '1/5' | '1/4' | '1/3' | '1/2' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
@@ -64,23 +90,15 @@ type MarkLabel = '1/9' | '1/8' | '1/7' | '1/6' | '1/5' | '1/4' | '1/3' | '1/2' |
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+    //#region properties
     public altControl = new FormControl('');
-    public alternatives: string[] = ['germany', 'poland', 'turkey'];
+    public alternatives: string[] = [];
     public criteriaControl = new FormControl('');
-    public criterias: string[] = [
-        'цiна',
-        'якiсть сировини',
-        'час доставки',
-        'надiйнiсть(% виконаних контрактiв)',
-        'сервiс та пiдтримка',
-        'лог. iнфраструктура',
-    ];
+    public criterias: string[] = [];
 
-    public criteriaComparisonMatrix: IComparison[] = [];
-    public criteriaComparisonDone: boolean = false;
-
-    public altComparisonMatrixes: IComparisonByCriterion[] = [];
-    public altComparisonDone: boolean = false;
+    public criteriaComparisonMatrix: ICrCompMatrix = null;
+    public altComparison: IAltComparison = null;
+    public results: IResult[] = null;
 
     // Comparison matrix properties
     public markLabels = ['1/9', '1/8', '1/7', '1/6', '1/5', '1/4', '1/3', '1/2', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -103,10 +121,43 @@ export class AppComponent {
         '8': 8,
         '9': 9,
     };
+    //#endregion properties
 
     constructor(private snackBar: MatSnackBar) {}
 
-    //#region Step 1: Alternatives
+    public onStepChange(event: StepperSelectionEvent): void {
+        if (event.selectedIndex === 1) {
+            if (!this.criteriaComparisonMatrix) {
+                this.criteriaComparisonMatrix = this.createCriteriaComparisonMatrix();
+            }
+        }
+        if (event.selectedIndex === 2) {
+            if (!this.altComparison) {
+                this.altComparison = this.createAltComparison();
+            }
+        }
+        if (event.selectedIndex === 3) {
+            this.results = [];
+            for (let altI = 0; altI < this.alternatives.length; altI++) {
+                const resRow: IResult = {
+                    alternative: this.alternatives[altI],
+                    priorityVector: [],
+                    result: 0,
+                };
+                for (let mI = 0; mI < this.altComparison.matrixes.length; mI++) {
+                    resRow.priorityVector.push(this.altComparison.matrixes[mI].result[altI].geomMeanNormalized);
+                }
+                resRow.result = resRow.priorityVector.reduce(
+                    (sum, item, index) => sum + item * this.criteriaComparisonMatrix.result[index].geomMeanNormalized,
+                    0
+                );
+                this.results.push(resRow);
+            }
+            console.log(this.results);
+        }
+    }
+
+    //#region Step 1: Alternatives And Criterias
     public onRemoveAlt(alt: string) {
         this.alternatives = this.alternatives.filter((x) => x !== alt);
     }
@@ -120,9 +171,7 @@ export class AppComponent {
 
         event.chipInput!.clear();
     }
-    //#endregion
 
-    //#region Step 2: Criteria
     public onRemoveCriteria(cr: string) {
         this.criterias = this.criterias.filter((x) => x !== cr);
     }
@@ -138,13 +187,23 @@ export class AppComponent {
     }
     //#endregion
 
-    //#region Step 3: Criteria Comparison Matrix
-    public onStep2Finish() {
-        this.criteriaComparisonMatrix = this.createCriteriaComparisonMatrix();
-    }
+    //#region Step 2: Criteria Comparison Matrix
 
-    private createCriteriaComparisonMatrix(): IComparison[] {
-        const matrix: IComparison[] = [];
+    private createCriteriaComparisonMatrix(): ICrCompMatrix {
+        const matrix: ICrCompMatrix = {
+            compValues: [],
+            processed: false,
+            consistent: false,
+            priorityVector: null,
+            weightedSumVector: [],
+            consistencyVector: [],
+            result: [],
+            lambdaMax: null,
+            ci: null,
+            ri: null,
+            cr: null,
+            threshold: null,
+        };
         for (let i = 0; i < this.criterias.length; i++) {
             const row: IComparison = {
                 entity: this.criterias[i],
@@ -177,110 +236,53 @@ export class AppComponent {
                     });
                 }
             }
-            matrix.push(row);
+            matrix.compValues.push(row);
         }
         return matrix;
     }
 
     public onImportanceSelect(rowIndex: number, colIndex: number, value: MarkLabel): void {
         // Update selected cell
-        this.criteriaComparisonMatrix[rowIndex].importances[colIndex].valueLabel = value;
-        this.criteriaComparisonMatrix[rowIndex].importances[colIndex].value = this.markValues[value];
+        this.criteriaComparisonMatrix.compValues[rowIndex].importances[colIndex].valueLabel = value;
+        this.criteriaComparisonMatrix.compValues[rowIndex].importances[colIndex].value = this.markValues[value];
 
         // Update reciprocal cell (lower triangle)
         const reciprocalLabel = this.getReciprocalMarkLabel(value);
-        this.criteriaComparisonMatrix[colIndex].importances[rowIndex].valueLabel = reciprocalLabel;
-        this.criteriaComparisonMatrix[colIndex].importances[rowIndex].value = this.markValues[reciprocalLabel];
+        this.criteriaComparisonMatrix.compValues[colIndex].importances[rowIndex].valueLabel = reciprocalLabel;
+        this.criteriaComparisonMatrix.compValues[colIndex].importances[rowIndex].value = this.markValues[reciprocalLabel];
     }
 
-    public onCheckCriteriaComparison() {
-        const criteriaWeightsData = this.calculateWeights(this.criteriaComparisonMatrix);
-
-        const crResult = this.checkConsistencyRatio(this.criteriaComparisonMatrix, criteriaWeightsData);
-        if (crResult.isConsistent) {
-            this.criteriaComparisonDone = true;
-            this.snackBar.open(`Matrix is consistent. ${crResult.info}`, 'Ok', {
-                duration: 5000,
-            });
-        } else {
-            this.snackBar.open(`Matrix is inconsistent. ${crResult.info}.\n Please, recompare!`, 'Ok', {
-                duration: 5000,
-            });
-            this.createCriteriaComparisonMatrix();
-        }
+    public onProcessCriteriaComparison() {
+        this.processComparisonMatrix(this.criteriaComparisonMatrix);
     }
+
     //#endregion
 
-    //#region Step 4: Alternatives Compare Matrixes
-    public onCompareAlts(): void {
-        this.altComparisonMatrixes = this.createAltComparisonMatrixes();
-    }
+    //#region Step 3: Alternatives Compare Matrixes
 
-    public onAltImportanceSelect(mIndex: number, rowIndex: number, colIndex: number, value: MarkLabel): void {
-        // Update selected cell
-        this.altComparisonMatrixes[mIndex].comparisons[rowIndex].importances[colIndex].valueLabel = value;
-        this.altComparisonMatrixes[mIndex].comparisons[rowIndex].importances[colIndex].value = this.markValues[value];
-
-        // Update reciprocal cell (lower triangle)
-        const reciprocalLabel = this.getReciprocalMarkLabel(value);
-        this.altComparisonMatrixes[mIndex].comparisons[colIndex].importances[rowIndex].valueLabel = reciprocalLabel;
-        this.altComparisonMatrixes[mIndex].comparisons[colIndex].importances[rowIndex].value = this.markValues[reciprocalLabel];
-    }
-
-    public onAltCRcheck(mIndex: number): void {
-        const criteriaWeightsData = this.calculateWeights(this.altComparisonMatrixes[mIndex].comparisons);
-
-        const crResult = this.checkConsistencyRatio(this.altComparisonMatrixes[mIndex].comparisons, criteriaWeightsData);
-        if (crResult.isConsistent) {
-            this.altComparisonMatrixes[mIndex].consistent = true;
-            this.snackBar.open(`Matrix is consistent. ${crResult.info}`, 'Ok', {
-                duration: 5000,
-            });
-        } else {
-            this.altComparisonMatrixes[mIndex].consistent = false;
-            this.snackBar.open(`Matrix is inconsistent. ${crResult.info}. Please, recompare!`, 'Ok', {
-                duration: 5000,
-            });
-        }
-    }
-
-    public onCheckAltsComparisons(): void {
-        console.log(this.altComparisonMatrixes);
-        for (let i = 0; i < this.altComparisonMatrixes.length; i++) {
-            const weights = this.calculateWeights(this.altComparisonMatrixes[i].comparisons);
-            const crResult = this.checkConsistencyRatio(this.altComparisonMatrixes[i].comparisons, weights);
-            this.altComparisonMatrixes[i].consistent = crResult.isConsistent;
-        }
-        const inconsistentAltsComparisonMatrixes = this.altComparisonMatrixes.filter((m) => !m.consistent);
-        console.log(inconsistentAltsComparisonMatrixes);
-        if (inconsistentAltsComparisonMatrixes.length > 0) {
-            this.snackBar.open(
-                `Some matrixes are inconsistent. Please, recompare: ${inconsistentAltsComparisonMatrixes
-                    .map((x) => x.criterion)
-                    .join(', ')}`,
-                'Ok',
-                { duration: 5000 }
-            );
-        } else {
-            this.altComparisonDone = true;
-        }
-    }
-    //#endregion
-
-    //#region Step 5: Results
-    public onResultsStep(): void {}
-    //#endregion
-
-    //#region helpers
-    private createAltComparisonMatrixes(): IComparisonByCriterion[] {
-        const matrix: IComparisonByCriterion[] = [];
+    private createAltComparison(): IAltComparison {
+        const comparison: IAltComparison = {
+            matrixes: [],
+            processed: null,
+            consistent: null,
+        };
 
         // For each criterion, create a comparison matrix of alternatives
         for (let i = 0; i < this.criterias.length; i++) {
-            const comparisonByCriterion: IComparisonByCriterion = {
+            const matrix: IAltCompMatrix = {
                 criterion: this.criterias[i],
+                compValues: [],
+                processed: false,
                 consistent: false,
-                comparisons: [],
+                priorityVector: null,
+                weightedSumVector: [],
+                consistencyVector: [],
+                result: [],
+                lambdaMax: null,
+                ci: null,
+                ri: null,
+                cr: null,
+                threshold: null,
             };
 
             for (let n = 0; n < this.alternatives.length; n++) {
@@ -315,72 +317,42 @@ export class AppComponent {
                         });
                     }
                 }
-                comparisonByCriterion.comparisons.push(row);
+                matrix.compValues.push(row);
             }
-            matrix.push(comparisonByCriterion);
+            comparison.matrixes.push(matrix);
         }
-        return matrix;
+        return comparison;
     }
 
-    private calculateWeights(array: IComparison[]): WeightResult[] {
-        const criteriaWeights = array.map((criterionData) => {
-            const product = criterionData.importances.reduce((acc: number, importance: any) => {
-                return acc * importance.value;
-            }, 1);
+    public onAltImportanceSelect(mIndex: number, rowIndex: number, colIndex: number, value: MarkLabel): void {
+        // Update selected cell
+        this.altComparison.matrixes[mIndex].compValues[rowIndex].importances[colIndex].valueLabel = value;
+        this.altComparison.matrixes[mIndex].compValues[rowIndex].importances[colIndex].value = this.markValues[value];
 
-            const geomMean = this.nthRoot(product, array.length);
-            return { entity: criterionData.entity, geomMean: geomMean };
-        });
-
-        const sumGeomMeans = criteriaWeights.reduce((sum, item) => sum + item.geomMean, 0);
-
-        const result = criteriaWeights.map((item) => ({
-            entity: item.entity,
-            geomMean: item.geomMean,
-            geomMeanNormalized: item.geomMean / sumGeomMeans,
-        }));
-
-        return result;
+        // Update reciprocal cell (lower triangle)
+        const reciprocalLabel = this.getReciprocalMarkLabel(value);
+        this.altComparison.matrixes[mIndex].compValues[colIndex].importances[rowIndex].valueLabel = reciprocalLabel;
+        this.altComparison.matrixes[mIndex].compValues[colIndex].importances[rowIndex].value = this.markValues[reciprocalLabel];
     }
 
-    private checkConsistencyRatio(comparisonMatrix: IComparison[], weightsData: WeightResult[]): { isConsistent: boolean; info: string } {
-        const n = comparisonMatrix.length;
-        const RI = this.getRandomIndex(n);
-        const weights = weightsData.map((w) => w.geomMeanNormalized);
-
-        // 1. Calculate Weighted Sum Vector (WSV)
-        const weightedSumVector = comparisonMatrix.map((row) => {
-            return row.importances.reduce((sum, imp, j) => sum + imp.value * weights[j], 0);
-        });
-
-        // 2. Compute Consistency Vector (CV)
-        const consistencyVector = weightedSumVector.map((ws, i) => ws / weights[i]);
-
-        // 3. Calculate λ_max (lambda max)
-        const lambdaMax = consistencyVector.reduce((sum, cv) => sum + cv, 0) / n;
-
-        // 4. Compute Consistency Index (CI)
-        const CI = (lambdaMax - n) / (n - 1);
-
-        // 5. Calculate Consistency Ratio (CR)
-        const CR = CI / RI;
-
-        // Determine threshold based on matrix size
-        let threshold: number;
-        if (n === 3) {
-            threshold = 0.05;
-        } else if (n === 4) {
-            threshold = 0.08;
-        } else {
-            threshold = 0.1;
-        }
-
-        if (CR < threshold) {
-            return { isConsistent: true, info: `${CR} < ${threshold}` };
-        } else {
-            return { isConsistent: false, info: `${CR} ≥ ${threshold}` };
-        }
+    public onProcessAltComparison(mIndex: number): void {
+        this.processComparisonMatrix(this.altComparison.matrixes[mIndex]);
     }
+
+    public onProcessAltsComparisons(): void {
+        for (let i = 0; i < this.altComparison.matrixes.length; i++) {
+            this.processComparisonMatrix(this.altComparison.matrixes[i]);
+        }
+        const consistent = this.altComparison.matrixes.filter((x) => !x.consistent);
+        this.altComparison.consistent = consistent.length === 0;
+    }
+
+    //#endregion
+
+    //#region Step 4: Results
+    //#endregion
+
+    //#region helpers
 
     private getReciprocalMarkLabel(label: MarkLabel): MarkLabel {
         const reciprocalMap: Record<MarkLabel, MarkLabel> = {
@@ -418,12 +390,79 @@ export class AppComponent {
             8: 1.41,
             9: 1.45,
             10: 1.49,
+            11: 1.51,
+            12: 1.48,
+            13: 1.56,
+            14: 1.57,
+            15: 1.59,
         };
-        return riValues[n] || 1.49; // Default for n > 10
+        return riValues[n] || 1.49;
     }
 
     private nthRoot(x: number, n: number) {
         return Math.pow(x, 1 / n);
+    }
+
+    private processComparisonMatrix(matrix: ICrCompMatrix): void {
+        const criteriaWeights = matrix.compValues.map((criterionData) => {
+            const product = criterionData.importances.reduce((acc: number, importance: any) => {
+                return acc * importance.value;
+            }, 1);
+
+            const geomMean = this.nthRoot(product, matrix.compValues.length);
+            return { entity: criterionData.entity, geomMean: geomMean };
+        });
+
+        const sumGeomMeans = criteriaWeights.reduce((sum, item) => sum + item.geomMean, 0);
+
+        matrix.result = criteriaWeights.map((item) => ({
+            entity: item.entity,
+            geomMean: item.geomMean,
+            geomMeanNormalized: item.geomMean / sumGeomMeans,
+        }));
+
+        //// CHECK CR
+        const n = matrix.compValues.length;
+        const RI = this.getRandomIndex(n);
+        const weights = matrix.result.map((w) => w.geomMeanNormalized);
+
+        // 1. Calculate Weighted Sum Vector (WSV)
+        const weightedSumVector = matrix.compValues.map((row) => {
+            return row.importances.reduce((sum, imp, j) => sum + imp.value * weights[j], 0);
+        });
+
+        // 2. Compute Consistency Vector (CV)
+        const consistencyVector = weightedSumVector.map((ws, i) => ws / weights[i]);
+
+        // 3. Calculate λ_max (lambda max)
+        const lambdaMax = consistencyVector.reduce((sum, cv) => sum + cv, 0) / n;
+
+        // 4. Compute Consistency Index (CI)
+        const CI = (lambdaMax - n) / (n - 1);
+
+        // 5. Calculate Consistency Ratio (CR)
+        const CR = CI / RI;
+
+        // Determine threshold based on matrix size
+        let threshold: number;
+        if (n === 3) {
+            threshold = 0.05;
+        } else if (n === 4) {
+            threshold = 0.08;
+        } else {
+            threshold = 0.1;
+        }
+        matrix.priorityVector = matrix.result.map((x) => x.geomMeanNormalized.toFixed(8)).join(', ');
+        matrix.weightedSumVector = weightedSumVector;
+        matrix.consistencyVector = consistencyVector;
+        matrix.lambdaMax = lambdaMax;
+        matrix.ci = CI;
+        matrix.ri = RI;
+        matrix.cr = CR;
+        matrix.threshold = threshold;
+
+        matrix.consistent = CR < threshold;
+        matrix.processed = true;
     }
 
     //#endregion
